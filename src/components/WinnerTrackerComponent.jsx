4ed8996bff3ebb59493d1763c6db7e0a6e83a6c1
Brian Angulo <brianangulo96@gmail.com>
2021-06-15 23:40:58 -0400

    Winners screen

--- src/components/WinnerTrackerComponent.jsx
+++ src/components/WinnerTrackerComponent.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import { Typography, Grid, makeStyles, Button } from "@material-ui/core";
+import { Typography, Grid, makeStyles, Divider } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    margin: {
-        margin: theme.spacing(0.2),
-    },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  hr: {
+    height: 2,
+    width: 400,
+    margin: theme.spacing(1),
+    backgroundColor: "#FFFFFF",
+  },
 }));
 
 function ScoreTracker() {
     const classes = useStyles();
         return (
-            <Grid className={classes.margin} container>
-                <Grid item>
-                    <Typography>
-                        Score: 
-                    </Typography>
-                </Grid>
+          <>
+            <Grid className={classes.margin} container justify="center">
+              <Grid item>
+                  <Divider light className={classes.hr} />
+                <Typography variant="h5">Current Winner: Tony Stark</Typography>
+              </Grid>
             </Grid>
+            <Grid className={classes.margin} container justify="center">
+              <Grid item>
+                <Typography variant="h5">Points Ahead: 4</Typography>
+              </Grid>
+            </Grid>
+          </>
         );
 }
 
--- src/screens/WinnersInputScreen.js
+++ src/screens/WinnersInputScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PlayerTrackerComponent from "../components/PlayerTrackerComponent";
+import ScoreTracker from "../components/WinnerTrackerComponent";
 
 function WinnersInputScreen() {
   return (
     <>
       <PlayerTrackerComponent />
+      <ScoreTracker />
     </>
   );
 }
